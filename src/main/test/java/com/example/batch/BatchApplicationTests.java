8c97bc4d8582c1d549604d8032e850c87e383922
kannanbe-code <kannan896@gmail.com>
2025-07-19 21:02:24 +0530

    Update BatchApplicationTests.java

--- src/main/test/java/com/example/batch/BatchApplicationTests.java
+++ src/main/test/java/com/example/batch/BatchApplicationTests.java
@@ -1,11 +1,31 @@
 package com.example.batch;
 
+import com.example.batch.config.BatchConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BatchApplicationTests {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job restSubmitJob;
+
     @Test
-    void contextLoads() {
+    void testJobExecution() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("timestamp", System.currentTimeMillis())
+                .toJobParameters();
+
+        JobExecution jobExecution = jobLauncher.run(restSubmitJob, jobParameters);
+
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
     }
 }
