dd3a78a97998c7bc1d1acf63ae90dc9631ea24ef
kannanbe-code <kannan896@gmail.com>
2025-07-21 13:19:43 +0530

    Update CustomerOrderProductDTO.java

--- src/main/java/com/example/batch/model/CustomerOrderProductDTO.java
+++ src/main/java/com/example/batch/model/CustomerOrderProductDTO.java
@@ -1,10 +1,25 @@
 package com.example.batch.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * This class represents a DTO that holds combined data
  * from multiple joined tables such as orders, customers, and products.
  * It's used to serialize/deserialize data between DB and REST API.
+ *
+ * Represents a customer record fetched from the database.
+ * 
+ * This model is used as the input and output of the batch step.
+ * Lombok Annotations:
+ * - @Data: Generates getters, setters, equals, hashCode, toString.
+ * - @NoArgsConstructor: Default constructor.
+ * - @AllArgsConstructor: Constructor with all fields.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerOrderProductDTO {
 
     private Long orderId;
@@ -13,63 +28,5 @@ public class CustomerOrderProductDTO {
     private Integer quantity;
     private Double price;
 
-    public CustomerOrderProductDTO() {
-        // Default constructor
-    }
 
-    // Getters and Setters
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getProductCode() {
-        return productCode;
-    }
-
-    public void setProductCode(String productCode) {
-        this.productCode = productCode;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    /**
-     * For logging failed items or debugging purposes.
-     */
-    @Override
-    public String toString() {
-        return "CustomerOrderProductDTO{" +
-                "orderId=" + orderId +
-                ", customerName='" + customerName + '\'' +
-                ", productCode='" + productCode + '\'' +
-                ", quantity=" + quantity +
-                ", price=" + price +
-                '}';
-    }
 }
