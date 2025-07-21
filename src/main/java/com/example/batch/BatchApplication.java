3a8fbd41f5d7a845b43100000ca2c68b8bc059c4
kannanbe-code <kannan896@gmail.com>
2025-07-21 13:13:31 +0530

    Update BatchApplication.java

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * Main entry point for the Spring Boot application.
  * Also enables Spring scheduling.
+ *- Uses @EnableScheduling to activate Spring's scheduled task execution.
  */
 @SpringBootApplication
 @EnableScheduling
